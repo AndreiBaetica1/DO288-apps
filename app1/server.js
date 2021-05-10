15058bf1378be5f0e1912d72e82b4b7292e2d225
AndreiBaetica1 <83407799+AndreiBaetica1@users.noreply.github.com>
2021-05-10 10:57:17 +0300

    Update server.js

--- app1/server.js
+++ app1/server.js
@@ -7,10 +7,10 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: ,
- user: ,
- password: ,
- database: 
+ host: root,
+ user: andrei,
+ password: Password,
+ database: database1
 });
 con.connect(function(err) {
  if (err) throw err;
